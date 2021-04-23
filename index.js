02064822a54c1d37a20720c201d8bf91d39aac2d
alexey432 <alexorl432@gmail.com>
2021-04-23 14:46:21 +0300

    heroku deployment

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-app.listen(5001, () => console.log('Listening at 5001'));
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log('Listening at 5001'));
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
 
